335befefea6f472b90080324a82832b6822b6fcd
Lars Johannsmeier <lars.johannsmeier@tum.de>
2020-06-09 13:25:11 -0700

    added unit test file for json.cpp

---
+++ cpp/unit_tests/test_json.cpp
@@ -0,0 +1,17 @@
+#include "msrm_utils/json.hpp"
+#include "catch/catch.hpp"
+
+namespace msrm_utils{
+
+TEST_CASE("convert to json","[json]"){
+
+    Eigen::Matrix<double,4,1> a;
+    a<<1,2,3,4;
+    nlohmann::json a_json;
+    a_json={1,2,3,4};
+
+    REQUIRE(from_eigen<double,4,1>(a)==a_json);
+
+}
+
+}
