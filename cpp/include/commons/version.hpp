6794acff9c8731003f13b22a1372e98204ad7d19
Lars Johannsmeier <lars.johannsmeier@tum.de>
2021-04-29 10:32:04 +0200

    build pipeline overhaul

---
+++ cpp/include/commons/version.hpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 - present, Lars Johannsmeier
+// All rights reserved.
+// contact: lars.johannsmeier@gmail.com
+
+#pragma once
+
+#define MSRM_UTILS_VER_MAJOR 1
+#define MSRM_UTILS_VER_MINOR 0
+#define MSRM_UTILS_VER_PATCH 3
+
+#define MSRM_UTILS_VERSION (MSRM_UTILS_VER_MAJOR * 10000 + MSRM_UTILS_VER_MINOR * 100 + MSRM_UTILS_VER_PATCH)
