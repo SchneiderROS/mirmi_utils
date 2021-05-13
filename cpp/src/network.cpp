7cd0bf7238415070f2f0017306bd41fa3fe09a24
Lars Johannsmeier <lars.johannsmeier@tum.de>
2021-05-13 15:36:30 +0200

    added function to send strings via UDP

--- cpp/src/network.cpp
+++ cpp/src/network.cpp
@@ -832,7 +832,7 @@ bool UDPStreamSender::disconnect(){
     return true;
 }
 
-bool UDPStreamSender::send(const std::vector<double> payload){
+bool UDPStreamSender::send(const std::vector<double>& payload){
     // Determine package size: Size of payload * 4 (4 bytes per double) + 4 start bytes + 4 end bytes + package counter + size of payload
     unsigned package_size=payload.size()*4+m_header_size;
 
@@ -879,6 +879,18 @@ bool UDPStreamSender::send(const std::vector<double> payload){
     msg[cnt_byte++]=126;
     msg[cnt_byte++]=126;
     msg[cnt_byte++]=126;
+
+    // The message is sent out to the peer robot.
+    int err=sendto(m_socket, msg, sizeof(msg) , 0 , (struct sockaddr *) &m_si_other, m_slen)<0;
+    if(err<0){ // If an error occured during sending...
+        std::cout<<"Could not send message: "<<std::strerror(errno)<<std::endl;
+        return false;
+    }
+    return true;
+}
+
+bool UDPStreamSender::send(const std::string &payload){
+    const char* msg = payload.c_str();
 
     // The message is sent out to the peer robot.
     int err=sendto(m_socket, msg, sizeof(msg) , 0 , (struct sockaddr *) &m_si_other, m_slen)<0;
