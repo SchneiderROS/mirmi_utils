0346b1a3319cbeb4335f7736f0e76fe36c2cecdd
Lars Johannsmeier <lars.johannsmeier@tum.de>
2021-05-14 10:35:20 +0200

    added options to skip example and test building

--- cpp/cmake/extern.cmake
+++ cpp/cmake/extern.cmake
@@ -3,5 +3,8 @@ FetchContent_Declare(
     GIT_REPOSITORY https://github.com/jsonrpcx/json-rpc-cxx.git
     GIT_TAG v0.3.0)
 
+set(COMPILE_TESTS OFF CACHE INTERNAL "Build SHARED libraries")
+set(COMPILE_EXAMPLES OFF CACHE INTERNAL "Build SHARED libraries")
+
 FetchContent_MakeAvailable(jsonrpc)
 
