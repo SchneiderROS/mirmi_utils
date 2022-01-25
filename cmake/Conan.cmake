f86df03dc5e85db33e692d902c9b66dc7caf99c6
Dennis Knobbe <dennis.knobbe@tum.de>
2022-01-25 15:17:25 +0100

    Fix conan packages to the right versions

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -1,19 +1,7 @@
 if(NOT EXISTS "${PROJECT_BINARY_DIR}/conan.cmake")
     message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
     file(DOWNLOAD  "https://raw.githubusercontent.com/conan-io/cmake-conan/master/conan.cmake"
-        "${PROJECT_BINARY_DIR}/conan.cmake"
-        STATUS DOWNLOAD_STATUS)
-    # Separate the returned status code, and error message.
-    list(GET DOWNLOAD_STATUS 0 STATUS_CODE)
-    list(GET DOWNLOAD_STATUS 1 ERROR_MESSAGE)
-    # Check if download was successful.
-    if(${STATUS_CODE} EQUAL 0)
-      message(STATUS "Download completed successfully!")
-    else()
-      # Exit CMake if the download failed, printing the error message.
-      message(FATAL_ERROR "Error occurred during download: ${ERROR_MESSAGE}")
-    endif()
-
+        "${PROJECT_BINARY_DIR}/conan.cmake")
 endif()
 include(${PROJECT_BINARY_DIR}/conan.cmake)
 
